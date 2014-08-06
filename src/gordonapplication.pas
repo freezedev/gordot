1c7a9e216a77878abd7bdde0bf43471937b8f0e2
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-06 22:42:48 +0200

    TGDApplication stub

---
+++ src/gordonapplication.pas
@@ -0,0 +1,20 @@
+unit GordonApplication;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TGDApplication = class(TObject)
+  public
+    constructor Create;
+    destructor Destroy; Override;
+  end;
+
+implementation
+
+end.
+
