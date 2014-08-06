e94086731154174dcb67fd2eef140ca0e45a94e0
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-06 22:56:07 +0200

    Width and Height properties

--- src/gordondisplayobject.pas
+++ src/gordondisplayobject.pas
@@ -8,11 +8,31 @@ uses
   Classes, SysUtils;
 
 type
-  TGDDisplayObject = class abstract(TObject)
 
+  { TGDDisplayObject }
+
+  TGDDisplayObject = class abstract(TObject)
+  private
+    function GetWidth(): Single;
+    function GetHeight(): Single;
+  published
+    property Width: Single read GetWidth;
+    property Height: Single read GetHeight;
   end;
 
 implementation
+
+{ TGDDisplayObject }
+
+function TGDDisplayObject.GetWidth: Single;
+begin
+  Result := 0;
+end;
+
+function TGDDisplayObject.GetHeight: Single;
+begin
+  Result := 0;
+end;
 
 end.
 
