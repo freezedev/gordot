ad16aeb4fa96b559c24215fe588ba8af7145e7a4
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-06 22:55:56 +0200

    Fixes compiler error

--- src/gordonapplication.pas
+++ src/gordonapplication.pas
@@ -5,16 +5,44 @@ unit GordonApplication;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, GordonSprite, GordonDisplayObject;
 
 type
+
+  { TGDApplication }
+
   TGDApplication = class(TObject)
+  private
+    fRoot: TGDSprite;
   public
     constructor Create;
     destructor Destroy; Override;
+
+    procedure AddChild(DisplayObject: TGDDisplayObject);
   end;
 
 implementation
+
+{ TGDApplication }
+
+constructor TGDApplication.Create;
+begin
+  inherited Create;
+
+  fRoot := TGDSprite.Create;
+end;
+
+destructor TGDApplication.Destroy;
+begin
+  fRoot.Destroy;
+
+  inherited Destroy;
+end;
+
+procedure TGDApplication.AddChild(DisplayObject: TGDDisplayObject);
+begin
+  fRoot.AddChild(DisplayObject);
+end;
 
 end.
 
