a30183460f1aec237fc18145053dd159d8bbe667
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-06 22:42:23 +0200

    TGDDisplayObject stub

---
+++ src/gordondisplayobject.pas
@@ -0,0 +1,18 @@
+unit GordonDisplayObject;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+type
+  TGDDisplayObject = class abstract(TObject)
+
+  end;
+
+implementation
+
+end.
+
