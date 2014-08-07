eacc6cbc8402d3cd2085f5503fc327d6f5cb7868
Johannes Stein <johannesstein@freeze-dev.com>
2014-08-07 22:15:21 +0200

    TGDImage stub

---
+++ src/gordonimage.pas
@@ -0,0 +1,21 @@
+unit GordonImage;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, GordonDisplayObject;
+
+type
+
+  { TGDImage }
+
+  TGDImage = class(TGDDisplayOject)
+
+  end;
+
+implementation
+
+end.
+
